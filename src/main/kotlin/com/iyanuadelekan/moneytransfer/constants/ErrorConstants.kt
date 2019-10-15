a9ad12ea9b897c49eec5dc0c7380fdc5370edc59
SeunAdelekan <iyanuadelekan@gmail.com>
2019-10-15 14:52:52 +0100

    Chore: cleaned up error message.

--- src/main/kotlin/com/iyanuadelekan/moneytransfer/constants/ErrorConstants.kt
+++ src/main/kotlin/com/iyanuadelekan/moneytransfer/constants/ErrorConstants.kt
@@ -8,7 +8,7 @@ enum class RequestError(val code: String) {
 
 enum class ErrorMessage(val message: String) {
     UNSUPPORTED_CONTENT_TYPE("Unsupported HTTP content type"),
-    ACCOUNT_CREATION_PARAMS_REQUIRED("accountName and currency are required."),
+    ACCOUNT_CREATION_PARAMS_REQUIRED("Please input a valid accountName and currency."),
     INVALID_ACCOUNT_NAME("Account name must be a minimum of 3 characters in length."),
     INVALID_CURRENCY_NAME("That currency is not supported at the moment"),
     INVALID_ACCOUNT_ID("An account with that ID does not exist."),
@@ -16,6 +16,6 @@ enum class ErrorMessage(val message: String) {
     INVALID_PAGE_LIMIT("Limit must be greater than 0"),
     INVALID_SENDER_ACCOUNT_ID("A sender account with that ID does not exist."),
     INVALID_RECIPIENT_ACCOUNT_ID("A recipient account with that ID does not exist."),
-    TRANSACTION_OPERATION_PARAMS_REQUIRED("amount and currency are required."),
+    TRANSACTION_OPERATION_PARAMS_REQUIRED("Please input a valid amount and currency."),
     INVALID_TRANSACTION_AMOUNT("Transaction amounts must be greater than 0.00"),
 }
